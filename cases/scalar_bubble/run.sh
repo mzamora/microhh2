9e6903de29bd2141b05c35c2b3376b9774a2353d
mzamora <monicazamoraz@gmail.com>
2018-09-18 11:33:39 -0700

    fixing typo

--- cases/scalar_bubble/run.sh
+++ cases/scalar_bubble/run.sh
@@ -13,7 +13,7 @@ echo "Running scalar bubble w/zero scalars"
 $MICROHH_EXEC run scalar_bubble>>log
 echo "Introducing the scalar bubble and new .ini"
 cp scalar_bubble.ini scalar_bubble_restart.ini
-cp scalar_bubble.prog scalar_bubble_restart.prof
+cp scalar_bubble.prof scalar_bubble_restart.prof
 $PYTHON_EXEC make_scalar_bubble.py
 echo "Running from the scalar bubble onwards"
 $MICROHH_EXEC run scalar_bubble_restart>>log
